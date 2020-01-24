03643ae0a0795da9379baf5bc776aaa26dba5707
ins <zj.xu@maimemo.com>
2020-01-24 20:14:57 +0800

    feat: add mongodb drive

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export default {
+  "mongodb_url": "mongodb://127.0.0.1:27017/test",
+  "mongodb_connect_config": {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import express from 'express'
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
 import path from 'path';
 
+import config from './config';
 import router from './router';
+
+mongoose.connect(config.mongodb_url, config.mongodb_connect_config);
 
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
